a470477b5cee3d18c0b57ffcd7c693d55e0faca9
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-11-24 18:02:52 +0500

    Added authentication

--- pages/_app.js
+++ pages/_app.js
@@ -3,22 +3,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useTheme, ThemeProvider } from "@/context/ThemeContext";
 import Navbar from "@/components/Navbar";
 import { useEffect } from "react";
-
+import { ClerkProvider } from "@clerk/nextjs";
 export default function App({ Component, pageProps }) {
 
-useEffect(() => {
-	// Dynamically import Bootstrap JS only on the client side
-	if (typeof document !== "undefined") {
-		require("bootstrap/dist/js/bootstrap.bundle.min.js");
-	}
-}, []);
+	useEffect(() => {
+		// Dynamically import Bootstrap JS only on the client side
+		if (typeof document !== "undefined") {
+			require("bootstrap/dist/js/bootstrap.bundle.min.js");
+		}
+	}, []);
 
 
 	return (
 		<ThemeProvider>
 			<ThemeWrapper>
-				<Navbar />
-				<Component {...pageProps} />
+				<ClerkProvider>
+					<Navbar />
+					<Component {...pageProps} />
+				</ClerkProvider>
+
 			</ThemeWrapper>
 		</ThemeProvider>
 	);
